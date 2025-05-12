f180071f6ccbd1ef9c082da68b243fae612a8f7c
cCDragosh <102684362+cCDragosh@users.noreply.github.com>
2025-05-12 21:33:01 +0200

    Delete js/scripts.js

--- js/scripts.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.flip-container').forEach(container => {
-  container.addEventListener('click', () => {
-    container.querySelector('.flip-inner').classList.toggle('rotated');
-  });
-});
